3a0027385bfc314153b6898ee1a11f293a0c828c
Sangam Kumar <sangamkumar91@gmail.com>
2023-06-04 03:06:36 -0700

    Update client.py

--- client.py
+++ client.py
@@ -6,8 +6,8 @@ import errno
 
 HEADER_LENGTH = 10
 
-IP = "192.168.204.1"
-PORT = 5052
+IP = "127.0.0.1"
+PORT = 8080
 my_username = input("Username: ")
 
 # Create a socket
@@ -26,59 +26,64 @@ client_socket.setblocking(False)
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
-
+# Now we want to loop over received messages (there might be more than one) and print them
 while True:
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, client_socket]
+    
+    """ There are two possible input situations. Either the
+    user wants to give manual input to send to other people,
+    or the server is sending a message to be printed on the
+    screen. Select returns from sockets_list, the stream that
+    is reader for input. So for example, if the server wants
+    to send a message, then the if condition will hold true
+    below.If the user wants to send a message, the else
+    condition will evaluate as true"""
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    
+    for socks in read_sockets:
+        if socks == client_socket:
+            try:
+                username_header = client_socket.recv(HEADER_LENGTH)
 
-    # Wait for user to input a message
-    message = input(f'{my_username} > ')
+                # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+                if not len(username_header):
+                    print('Connection closed by the server')
+                    sys.exit()
 
-    # If message is not empty - send it
-    if message:
+                # Convert header to int value
+                username_length = int(username_header.decode('utf-8').strip())
 
-        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
+                # Receive and decode username
+                username = client_socket.recv(username_length).decode('utf-8')
 
-    try:
-        # Now we want to loop over received messages (there might be more than one) and print them
-        while True:
+                # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+                message_header = client_socket.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode('utf-8').strip())
+                message = client_socket.recv(message_length).decode('utf-8')
 
-            # Receive our "header" containing username length, it's size is defined and constant
-            username_header = client_socket.recv(HEADER_LENGTH)
+                # Print message
+                print(f'{username} > {message}')
+            except IOError as e:
+                # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+                # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+                # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+                # If we got different error code - something happened
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print('Reading error: {}'.format(str(e)))
+                    sys.exit()
 
-            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-            if not len(username_header):
-                print('Connection closed by the server')
+                # We just did not receive anything
+                continue
+
+            except Exception as e:
+                # Any other exception - something happened, exit
+                print('Reading error: '.format(str(e)))
                 sys.exit()
 
-            # Convert header to int value
-            username_length = int(username_header.decode('utf-8').strip())
-
-            # Receive and decode username
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            # Print message
-            print(f'{username} > {message}')
-
-    except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # We just did not receive anything
-        continue
-
-    except Exception as e:
-        # Any other exception - something happened, exit
-        print('Reading error: '.format(str(e)))
-        sys.exit()
+        else:
+            message = sys.stdin.readline()
+            encoded_message = message.encode('utf-8')
+            message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(message_header + encoded_message)
+            print(f'{my_username} > {message}')
